127bee556e267dac80d5efc1991b7fabc69b00a6
Angelique <angeliquebuijzen@hotmail.com>
2019-08-17 18:15:55 +0200

    MVC structuur toevoegen

--- routes/api/v1/chat.js
+++ routes/api/v1/chat.js
@@ -1,25 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const chatController = require('../../../controllers/api/v1/chat')
 
 /* /api/v1/chat */
 
-router.get("/",(req,res)=>{
-    res.json({
-        "status":"succes",
-        "data":{
-            "chat":[]
-        }
-    });
-});
+router.get("/", chatController.getAll);
 
-router.post("/", (req,res)=>{
-    res.json({
-        "status": "succes",
-        "data":{
-            "message":{
-                "text":"Learn Node js"}
-        }
-    });
-});
+router.post("/",chatController.create);
 
 module.exports = router;
