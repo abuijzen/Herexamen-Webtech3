cb9588c8ccf877a44f6e85f04d50ad332130ee5e
Angelique <angeliquebuijzen@hotmail.com>
2019-08-17 19:00:24 +0200

    foutmelding laten zien indien nieuwe message niet voldoet aan de voorwaarden

--- controllers/api/v1/chat.js
+++ controllers/api/v1/chat.js
@@ -4,7 +4,10 @@ const Schema = mongoose.Schema;
 
 /*mogelijke veldjes in mongodb*/
 const chatSchema = new Schema({
-    text: String,
+    text: {
+        type: String,
+        required:true}
+        ,
     user:String,
     completed:Boolean
 })
@@ -28,15 +31,28 @@ const getAll = (req,res)=>{
 
 
 
-
-const create = (req,res)=>{
+/* next kan de volgende middleware functie aanspreken*/
+const create = (req,res,next)=>{
 
     let message = new Message();
     message.text = "Eerste message";
     message.user ="Angelique";
     message.completed =false;
     message.save((err,doc)=>{
+        if(err){
+            /* express handeld te error verder af
+            Anders komt er geen foutmelding indien nodig
+            en blijft de applicatie vasthangen*/
+            // ->  return next(err);
 
+            /*als je niet wilt dat express de foutmelding geeft
+            maar je zelf een duidelijke boodschap wilt typen,
+            dan kan je een json object maken*/
+            res.json({
+                "status": "error",
+                "message":"Could not save this message"
+            });
+        }
         /*als er geen error is, wordt deze json terug gestuurd*/
         if(!err){
             res.json({
